c3839f08ed5233c64ad7cda8c20afdda8b875e25
mateoosz <gryzism@gmail.com>
2026-06-23 16:56:41 +0200

    second button past/present,

--- lib/engine/grammar_engine.dart
+++ lib/engine/grammar_engine.dart
@@ -1,15 +1,21 @@
 import '../models/sentence_state.dart';
 
+enum Subject { he, she, it, they }
+
+enum Verb { work, play, eat }
+
+enum Tense { past, present }
+
 class GrammarEngine {
   String generate(SentenceState state) {
-  if (
-    state.subject == 'he' &&
-    state.verb == 'work' &&
-    state.tense == 'past'
-  ) {
-    return 'He worked.';
-  }
+    if (state.tense == Tense.past) {
+      return 'He worked.';
+    }
 
-  return 'Unknown sentence';
+    if (state.tense == Tense.present) {
+      return 'He works.';
+    }
+
+    return 'Unknown sentence';
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,29 +3,51 @@ import 'package:flutter/material.dart';
 import '../engine/grammar_engine.dart';
 import '../models/sentence_state.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String tense = 'past';
+
+  @override
   Widget build(BuildContext context) {
-    final state = SentenceState(
-      subject: 'he',
-      verb: 'work',
-      tense: 'past',
-    );
+    final state = SentenceState(subject: 'he', verb: 'work', tense: tense);
 
     final grammarEngine = GrammarEngine();
 
     final sentence = grammarEngine.generate(state);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('English Padlock'),
-      ),
+      appBar: AppBar(title: const Text('English Padlock')),
       body: Center(
-        child: Text(
-          sentence,
-          style: const TextStyle(fontSize: 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(sentence, style: const TextStyle(fontSize: 32)),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  tense = 'present';
+                });
+              },
+              child: const Text('PRESENT'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  tense = 'past';
+                });
+              },
+              child: const Text('PAST'),
+            ),
+          ],
         ),
       ),
     );
